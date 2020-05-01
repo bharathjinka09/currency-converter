21872a0d83ac9c0feb7c733fcc836560b9f06189
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-01 22:04:53 +0530

    Updates

---
+++ precache-manifest.6a0b8a1731c503fb8a1f4354b80e7b92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc90dc13dad7860cbaf99d9c94dd0aa",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "6517b72908144e144425",
+    "url": "/currency-converter/static/css/2.1087be86.chunk.css"
+  },
+  {
+    "revision": "106ad88ee7c1d95904e5",
+    "url": "/currency-converter/static/css/main.33d7be06.chunk.css"
+  },
+  {
+    "revision": "6517b72908144e144425",
+    "url": "/currency-converter/static/js/2.42f9156e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.42f9156e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106ad88ee7c1d95904e5",
+    "url": "/currency-converter/static/js/main.6557f801.chunk.js"
+  },
+  {
+    "revision": "3950ea5715e8b793f0b9",
+    "url": "/currency-converter/static/js/runtime-main.dacde203.js"
+  }
+]);
